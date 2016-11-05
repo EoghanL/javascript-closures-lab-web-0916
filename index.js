fafb1fe2806c6b3be2be2c38c96e269eed89aa3f
github username <github email address>
2016-11-05 13:06:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const app = "I don't do much."
+var sharkCreator = createAnimal("Shark")
+var sharkWithFrickinLaserbeam = sharkCreator("Laserbeam")
+var sharkWithFrickinCannon = sharkCreator("Cannon")
+function bumpCounter(){
+  let counter = 0;
+
+  function getBumps(){
+    return counter;
+  }
+
+  function addBump(){
+    counter++;
+  }
+
+  return {
+    getBumps,
+    addBump
+  }
+}
+
+function createAnimal(animalType){
+  return function (deadlyDevice){
+    return { animalType: animalType, deadlyDevice: deadlyDevice}
+  }
+}
